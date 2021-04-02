2d149c88689eeb1ec9735cdcde8bf4e526866905
Nale322 <34818356+Nale322@users.noreply.github.com>
2021-04-03 00:17:25 +0300

    Update emailspammer.py

--- emailspammer.py
+++ emailspammer.py
@@ -5,10 +5,12 @@ from configparser import ConfigParser
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+from tkinter import *
+
 with open('info.csv', 'r', encoding='UTF-8') as file:
     reader = csv.DictReader(file, delimiter=',')
     data = [line for line in reader]
-print(data)
+
 config = ConfigParser()
 config.read('config.ini')
 
@@ -34,6 +36,59 @@ for key in data:
     msg['Subject'] = subject
     msg['From'] = login
 
-    server.sendmail(login, key['email'], msg.as_string())
+    #server.sendmail(login, key['email'], msg.as_string())
     sleep(timing)
 server.quit()
+
+def clicked():
+    lbl.configure(text = "Тебя ебали в детстве?")
+
+def edit_cfg():
+    num = txt.get()
+    print(num)
+    config.set('smtp', 'timing', (num))
+    with open("config.ini", "w") as f:
+        config.write(f)
+
+def add_user():
+    a = alo.get()
+    b = email.get()
+    with open('info.csv', 'a', encoding = 'UTF-8') as file:
+        file_writer = csv.writer(file, delimiter = ",", lineterminator = "\r")
+        file_writer.writerow([a, b])
+
+# Визуальная часть программы
+
+window = Tk()
+window.geometry('300x200')
+window.title("Настройка программы")
+window.resizable(False, False) 
+
+lbl = Label(window, text = "Введите число задержки:", font = ("Corbel"))
+lbl.grid(column = 0, row = 0)
+
+txt = Entry(window, width = 25)
+txt.grid(column = 0, row = 1)
+
+btn = Button(window, text = "Изменить", bg = "white", fg = "black", relief = "raised", command = edit_cfg)
+btn.grid(column = 1, row = 1)
+
+lbl = Label(window, text = "Добавить людей в рассылку:", font = ("Corbel"))
+lbl.grid(column = 0, row = 2)
+
+lbl = Label(window, text = "Введите имя человека:", font = ("Corbel"))
+lbl.grid(column = 0, row = 3)
+
+alo = Entry(window, width = 25)
+alo.grid(column = 0, row = 4)
+
+lbl = Label(window, text = "Введите email человека:", font = ("Corbel"))
+lbl.grid(column = 0, row = 5)
+
+email = Entry(window, width = 25)
+email.grid(column = 0, row = 6)
+
+btn = Button(window, text = "Добавить", bg = "white", fg = "black", relief = "groove", height = "2", command = add_user)
+btn.grid(column = 1, row = 5)
+
+window.mainloop()
